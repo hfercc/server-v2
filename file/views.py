925d1557bbfeeb8d39c52b4d35e3893eb93101ac
ChanYH <hfercxx@hotmail.com>
2018-09-05 14:04:44 +0800

    p

--- file/views.py
+++ file/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import FileResponse, Http404
+from django.http import FileResponse, Http404, HttpResponse
 
 from rest_framework.permissions import IsAuthenticated
 from utils.permissions import IsOwnerOrReadOnly
@@ -15,18 +15,36 @@ from rest_framework.response import Response
 from .models import FileRecord
 
 from .serializer import FileSerializer
+import re
+import csv
 
 # Create your views here.
 @decorators.api_view(['GET'])
 @decorators.permission_classes([permissions.IsAuthenticated])
 def download_key(request, report, file_name):
+    r = re.regex()
     user = request.user  
     if file_name:
         files = FileRecord.objects.filter(Q(author=user) & Q(report__alpha_name=report) & Q(name=file_name))
         if len(files) > 0:
-            ret = FileResponse(open(files[0].path, 'rb'))
-            filename = files[0].path.split('/')[-1]
-            ret['Content-Type']='application/octet-stream' if file_name != 'output_pnl.png' else 'image/jpeg'
+            if file_name == 'output_pnl.png':
+                ret = FileResponse(open(files[0].path, 'rb'))
+                filename = files[0].path.split('/')[-1]
+                ret['Content-Type'] = 'image/jpeg'
+            elif file_name == 'output_performance.csv':
+                ret = []
+                with open(files[0].path) as f:
+                    r = list(csv.reader(f))[0]
+                regex = re.compile('\s+')
+                columns = regex.split(r[0].strip())
+                columns[0] = 'period'
+                for i in range(1, len(r)):
+                    ret.append(zip(columns, regex.split(r[i].strip())))
+                return HttpResponse(ret)
+
+            else:
+                ret = FileResponse(open(files[0].path, 'rb'))
+                ret['Content-Type']='application/octet-stream'
             return ret
         else:
             raise Http404("File Not Found")
